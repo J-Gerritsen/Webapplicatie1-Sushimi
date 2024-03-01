400b5b140b42e131b22943107f37e1d46219be85
job-gerritsen <154206850+J-Gerritsen@users.noreply.github.com>
2024-03-01 12:22:47 +0100

    Created the homepage

---
+++ html/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@300..700&family=Lato:wght@700&family=Montserrat:wght@500;600;700&family=Open+Sans&family=Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li>
+
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+        <div class="hero-section-background">
+            <div class="hero-section">
+                <div class="hero-left-side">
+                    <div class="hero-title">
+                        <p>The most unique flavor experience with our fresh sushi that make you taste buds melt!</p>
+                    </div>
+                    <div class="hero-line">
+
+                    </div>
+                    <div class="hero-description">
+                        <p>Deliciously fresh sushi straight from Japan to give you the 
+    best flavor experience you have ever tasted</p>
+                    </div>
+                    <div class="hero-buttons">
+                        <div class="hero-our-menu-btn btn">
+                            <p>Our menu</p>
+                        </div>
+                        <div class="hero-book-a-table-btn btn">
+                            <p>Book a table</p>
+                        </div>
+                    </div>
+                    <div class="hero-our-reviews">
+                        
+                    </div>
+                </div>
+                <div class="hero-right-side">
+                    <img class="first-image-hero" src="assets/sushi1.jpg" alt="err 404">
+                    <img class="second-image-hero" src="assets/sushi2.jpg" alt="err 404">
+                    <img class="third-image-hero" src="assets/sushi3.jpg" alt="err 404">
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
